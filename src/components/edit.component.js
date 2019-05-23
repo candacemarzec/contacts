5e662a8c709dd1606163c5509a01e22683b53b87
candace <candace.marzec@gmail.com>
2019-05-23 10:03:06 -0700

    initial commit

---
+++ src/components/edit.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Edit extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to the Edit Component!!</p>
+            </div>
+        )
+    }
+}
